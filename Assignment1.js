da8e756e1901e5b92d35db1a557b907ed26c2871
don12122 <118923815+don12122@users.noreply.github.com>
2022-11-23 22:38:50 +0530

    Assignment1

---
+++ Assignment1.js
@@ -0,0 +1,33 @@
+
+function checkYear(year) {
+    if (year % 400 == 0)
+        return true;
+
+    if (year % 100 == 0)
+        return false;
+
+    if (year % 4 == 0)
+        return true;
+    return false;
+}
+
+function cToF(celsius) {
+    // to convert to 
+    var cTemp = celsius;
+    var cToFahr = cTemp * 9 / 5 + 32;
+    var message = cTemp + '\xB0C is ' + cToFahr + ' \xB0F.';
+    console.log(message);
+}
+
+function fToC(fahrenheit) {
+    var fTemp = fahrenheit;
+    var fToCel = (fTemp - 32) * 5 / 9;
+    var message = fTemp + '\xB0F is ' + fToCel + '\xB0C.';
+    console.log(message);
+}
+let year = 2010;
+let ans = year ? 'leap' : 'not leap';
+
+cToF(60);
+fToC(45);
+console.log(ans);
